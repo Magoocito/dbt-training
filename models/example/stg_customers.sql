b49f88011feb515640582af1e985e35bd1b5bf02
tatiana.rojas@encora.com <tatiana.rojas@encora.com>
2025-04-09 22:30:46 +0000

    stg_orders

--- models/example/stg_customers.sql
+++ models/example/stg_customers.sql
@@ -1,7 +1,7 @@
 With stg_customer AS (
   SELECT
 	Customer_id,
-	CONCAT(first_name, ‘ ‘, last_name) as customer_name,
+	CONCAT(first_name, '', last_name) as customer_name,
 	email as email_address,
 	address as billing_address
   FROM raw.customer
